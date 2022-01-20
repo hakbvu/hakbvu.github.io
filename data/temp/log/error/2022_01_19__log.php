93db9a881c4072e6ddf9995ef2373d5da17740f4
hakbvu <2727379352@qq.com>
2022-01-20 11:26:06 +0800

    create a project

---
+++ data/temp/log/error/2022_01_19__log.php
@@ -0,0 +1,7 @@
+<?php exit;?>
+[12:29:03.678]  ../app/autoload.php[2];  SourceModel->_addSoure(); ;Undefined constant "USER_ID"
+{
+    "0": "beforeShutdownFatalError("{229}#Error")",
+    "1": "app/autoload.php[2] think_exception("{229}#Error")",
+    "memory": "7.479M"
+}
---
+++ data/temp/log/lock/2022_01_19__log.php
@@ -0,0 +1,10 @@
+<?php exit;?>
+[12:29:03.683]  unlockRuntime:lock_aa1cb_ModelSource.lock.97a6bc1bf5cb6010cf82c7265b948a4f;{
+    "0": "beforeShutdown()",
+    "1": "app/autoload.php[2] Hook::trigger("beforeShutdown")",
+    "2": "app/autoload.php[2] Hook::apply("user.index.shutdownEvent",[])",
+    "3": "app/autoload.php[2] ActionApply("user.index.shutdownEvent",[])",
+    "4": "app/autoload.php[2] {16a}#userIndex->shutdownEvent()",
+    "5": "app/controller/user/index.class.php[37] CacheLock::unlockRuntime()",
+    "memory": "7.465M"
+}
